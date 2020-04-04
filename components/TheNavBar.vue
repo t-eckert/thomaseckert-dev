fed7c9ead2872ebd97014315f3b602b0b16a4929
theckert <thomas.james.eckert@gmail.com>
2020-04-04 14:48:53 -0700

    Add TheNavBar

---
+++ components/TheNavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="breadcrumbs">
+    <li v-for="(crumb, index) in breadcrumbs" :key="index">
+      {{ crumb.emoji }}
+      <nuxt-link :to="crumb.link" class="breadcrumb-link">{{
+        crumb.name
+      }}</nuxt-link>
+      <span class="breadcrumb-divider">&#47;</span>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { uiStore } from "@/store";
+
+export default Vue.extend({
+  name: "TheNavBar",
+
+  computed: {
+    breadcrumbs() {
+      return uiStore.breadcrumbs;
+    }
+  }
+});
+</script>
