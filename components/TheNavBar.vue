8833b0df31433787648b1d3c25b6a758ab1d76d4
theckert <thomas.james.eckert@gmail.com>
2020-04-13 13:30:19 -0700

    Add TheNavBar on default layout

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,26 +1,28 @@
 <template>
-  <ul class="breadcrumbs">
-    <li v-for="(crumb, index) in breadcrumbs" :key="index">
-      {{ crumb.emoji }}
-      <nuxt-link :to="crumb.link" class="breadcrumb-link">{{
-        crumb.name
-      }}</nuxt-link>
-      <span class="breadcrumb-divider">&#47;</span>
-    </li>
-  </ul>
+  <nav>
+    <ul class="breadcrumbs">
+      <li v-for="(crumb, index) in breadcrumbs" :key="index">
+        {{ crumb.emoji }}
+        <nuxt-link :to="crumb.link">{{ crumb.name }}</nuxt-link>
+        <span>&#47;</span>
+      </li>
+    </ul>
+    <div v-if="$auth.loggedIn" class="pill pill-red">
+      Admin
+    </div>
+  </nav>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import { uiStore } from "@/store";
 
 export default Vue.extend({
   name: "TheNavBar",
 
   computed: {
     breadcrumbs() {
-      return uiStore.breadcrumbs;
-    }
-  }
+      return this.$store.state.ui.breadcrumbs;
+    },
+  },
 });
 </script>
