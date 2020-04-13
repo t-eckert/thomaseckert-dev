57e46e6887abeeb03108a0d2c70ea2d99db9e3ee
theckert <thomas.james.eckert@gmail.com>
2020-04-13 13:31:26 -0700

    Add admin pages

---
+++ pages/admin/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <ThePostTable />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ThePostTable from "@/components/ThePostTable.vue";
+
+export default Vue.extend({
+  name: "TheAdminView",
+
+  middleware: ["auth"],
+
+  components: {
+    ThePostTable,
+  },
+
+  mounted() {
+    this.$store.commit("ui/SET_BREADCRUMBS", [
+      { emoji: "🏡", name: "Home", link: "/" },
+      { emoji: "🕶", name: "Admin", link: "/admin" },
+    ]);
+  },
+});
+</script>
