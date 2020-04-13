698d05ce8d057ab4ed79819cd4a614adcd3ccfb5
theckert <thomas.james.eckert@gmail.com>
2020-04-13 13:31:01 -0700

    Add about and favorites pages

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    About
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  components: {},
+
+  mounted() {
+    this.$store.commit("ui/SET_BREADCRUMBS", [
+      { emoji: "🏡", name: "Home", link: "/" },
+      { emoji: "💁‍♂️", name: "About", link: "/about" },
+    ]);
+  },
+});
+</script>
---
+++ pages/favorites.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    Favorites
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  components: {},
+
+  mounted() {
+    this.$store.commit("ui/SET_BREADCRUMBS", [
+      { emoji: "🏡", name: "Home", link: "/" },
+      { emoji: "✨", name: "Favorites", link: "/favorites" },
+    ]);
+  },
+});
+</script>
