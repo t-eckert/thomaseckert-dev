a21a50e869b4b00a3e4a56bc9d5a753195a6653e
theckert <thomas.james.eckert@gmail.com>
2020-05-03 14:01:10 -0700

    Add Mongoose model for Favorite

---
+++ models/Favorite.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import connection from "../utils/database-handler";
+
+const FavoriteSchema = new mongoose.Schema(
+  {
+    slug: String,
+    title: String,
+    markdown: String,
+  },
+  { collection: "Favorites" },
+);
+
+// Prevent duplication of PostModel
+if (connection.modelNames().includes("Favorite")) {
+  connection.deleteModel("Favorite");
+}
+
+const FavoriteModel = mongoose.model("Favorite", FavoriteSchema);
+
+export default FavoriteModel;
