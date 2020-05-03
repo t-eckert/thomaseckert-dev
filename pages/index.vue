bbde51fe659f0583f63fbeac2a18d2e2d78aac57
theckert <thomas.james.eckert@gmail.com>
2020-05-03 14:04:02 -0700

    Use asyncdata in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
   <section class="container grid-home section-home">
     <TheIntroduction />
-    <ThePostGallery />
+    <ThePostGallery :postsMetadata="postsMetadata" />
   </section>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import axios from "axios";
+import { host, routes } from "@/constants/";
 import TheIntroduction from "@/components/TheIntroduction.vue";
 import ThePostGallery from "@/components/ThePostGallery.vue";
 
 export default Vue.extend({
   components: { TheIntroduction, ThePostGallery },
+
+  /** Resolves a list of post metadata from the database */
+  async asyncData() {
+    const { data } = await axios.get(host + routes.POST_METADATA);
+    return { postsMetadata: data };
+  },
 
   mounted() {
     this.$store.commit("ui/SET_BREADCRUMBS", [
