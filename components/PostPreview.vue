732579e58f8317c3739a3309df0f4fd09439b94a
theckert <thomas.james.eckert@gmail.com>
2020-05-03 14:03:14 -0700

    PostPreview uses metadata from asyncdata

--- components/PostPreview.vue
+++ components/PostPreview.vue
@@ -1,34 +1,36 @@
 <template>
   <section>
-    <h3>{{ post.emoji }}</h3>
+    <h3>{{ postMetadata.emoji }}</h3>
     <h2 class="mb-2">
-      <nuxt-link :to="'/' + post.slug">{{ post.title }}</nuxt-link>
+      <nuxt-link :to="'/' + postMetadata.slug">{{
+        postMetadata.title
+      }}</nuxt-link>
     </h2>
     <div class="flex-meta mb-2">
-      <div>{{ formatDate(post.publishDate) }}</div>
+      <div>{{ formatDate(postMetadata.publishDate) }}</div>
       <div
-        v-for="(tag, index) in post.tags"
+        v-for="(tag, index) in postMetadata.tags"
         :key="index"
         class="pill pill-blue"
       >
         {{ tag }}
       </div>
     </div>
-    <p>{{ post.preview }}</p>
+    <p>{{ postMetadata.preview }}</p>
   </section>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Post from "@/interfaces/Post";
 import { formatDate } from "@/functions";
+import { PostMetadata } from "@/interfaces";
 
 export default Vue.extend({
   name: "PostPreview",
 
   props: {
-    post: {
-      type: Object as () => Post,
+    postMetadata: {
+      type: Object as () => PostMetadata,
       required: true,
     },
   },
