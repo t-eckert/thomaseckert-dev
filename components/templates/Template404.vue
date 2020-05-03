1998cdae6a443a48e4112667f2875e8bd9bfe09f
theckert <thomas.james.eckert@gmail.com>
2020-05-03 14:02:41 -0700

    Add Template404

---
+++ components/templates/Template404.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="display-1 text-center hero-text">404</h1>
+    <p class="text-center">It's going to be ok.</p>
+    <p class="text-center">🏡&nbsp;<nuxt-link to="/">Go home</nuxt-link></p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({});
+</script>
