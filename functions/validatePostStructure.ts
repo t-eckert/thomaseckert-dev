1787007ec3357525fdfea8539ef31ad50ff482f5
theckert <thomas.james.eckert@gmail.com>
2020-05-03 12:12:07 -0700

    Temporarily clear validate structure

--- functions/validatePostStructure.ts
+++ functions/validatePostStructure.ts
@@ -1,19 +1,18 @@
-import Post from "@/interfaces/Post";
+import { Post } from "@/interfaces";
 
 export const validatePostStructure = (post: Post): boolean => {
-  // todo 2020-04-05 this could be more elegant... finding the keys on the interface
-  const expectedKeys = [
-    "content",
-    "emoji",
-    "isPublished",
-    "preview",
-    "publishDate",
-    "slug",
-    "tags",
-    "title",
-  ];
-
-  const keys = Object.keys(post).sort();
-
-  return expectedKeys.toString() === keys.toString();
+  // // todo 2020-04-05 this could be more elegant... finding the keys on the interface
+  // const expectedKeys = [
+  //   "content",
+  //   "emoji",
+  //   "isPublished",
+  //   "preview",
+  //   "publishDate",
+  //   "slug",
+  //   "tags",
+  //   "title",
+  // ];
+  // const keys = Object.keys(post).sort();
+  // return expectedKeys.toString() === keys.toString();
+  return false;
 };
