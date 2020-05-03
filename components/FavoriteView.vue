349c71c29876016c90812d9cb35d6561149666f1
theckert <thomas.james.eckert@gmail.com>
2020-05-03 14:03:00 -0700

    Add FavoriteView

---
+++ components/FavoriteView.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <h2>{{ favorite.title }}</h2>
+    <hr />
+    <p class="mt-4 mb-8" v-html="body"></p>
+  </section>
+</template>
+
+<script>
+import marked from "marked";
+
+export default {
+  props: {
+    favorite: { required: true },
+  },
+
+  computed: {
+    body() {
+      return marked(this.favorite.markdown || "");
+    },
+  },
+};
+</script>
