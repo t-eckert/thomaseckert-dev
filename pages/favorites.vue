97b9a668df3a1ab06851c60d14ee3f6d96e429b1
theckert <thomas.james.eckert@gmail.com>
2020-05-03 14:04:43 -0700

    favorites uses asyncdata

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="container">
-    Favorites
+  <div class="container grid g-fit gg-4">
+    <Favorite
+      v-for="(favorite, index) in favorites"
+      :key="index"
+      :favorite="favorite"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import axios from "axios";
+import Favorite from "@/components/FavoriteView.vue";
+import { host, routes } from "@/constants";
 
 export default Vue.extend({
-  components: {},
+  components: { Favorite },
+
+  async asyncData() {
+    const { data: favorites } = await axios.get(host + routes.FAVORITES);
+    return {
+      favorites,
+    };
+  },
 
   mounted() {
     this.$store.commit("ui/SET_BREADCRUMBS", [
