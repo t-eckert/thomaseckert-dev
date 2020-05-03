eeb87ba3cd2559b3cfb4f9d976498028c0b1d671
theckert <thomas.james.eckert@gmail.com>
2020-05-03 13:57:57 -0700

    Add new favorite form

---
+++ components/forms/NewFavorite.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <form action="" method="PUT" class="flex">
+      <div class="flex-vertical">
+        <label for="title" class="mb-2">Title</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          v-model="title"
+          class="mb-4 mr-8"
+        />
+      </div>
+    </form>
+    <button @click="create()">Create</button>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      title: "",
+    };
+  },
+
+  methods: {
+    create() {
+      this.$store.dispatch("db/createNewFavorite", this.title);
+      this.$emit("created");
+    },
+  },
+});
+</script>
