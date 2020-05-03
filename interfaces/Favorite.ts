2d98afa1abfb4a4703c2806ff7d0ea716b50a29d
theckert <thomas.james.eckert@gmail.com>
2020-05-03 13:58:55 -0700

    Add `Favorite` interface

---
+++ interfaces/Favorite.ts
@@ -0,0 +1,5 @@
+export default interface Favorite {
+  slug: string;
+  title: string;
+  markdown: string;
+}
---
+++ interfaces/index.ts
@@ -0,0 +1,7 @@
+import Breadcrumb from "@/interfaces/Breadcrumb";
+import Favorite from "@/interfaces/Favorite";
+import Post from "@/interfaces/Post";
+import PostContent from "@/interfaces/PostContent";
+import PostMetadata from "@/interfaces/PostMetadata";
+
+export { Breadcrumb, Favorite, Post, PostContent, PostMetadata };
