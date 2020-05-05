73767545684bfea3da2bc46b89da4606c0a0838a
theckert <thomas.james.eckert@gmail.com>
2020-05-04 20:35:40 -0700

    Add auth to constants

--- constants/index.ts
+++ constants/index.ts
@@ -4,6 +4,7 @@ export const routes = {
   POST_CONTENT: "/api/posts/content/",
   POST_METADATA: "/api/posts/metadata/",
   FAVORITES: "/api/favorites/",
+  AUTH_TOKEN: "/api/auth/token",
 };
 
 export enum LoadStatus {
