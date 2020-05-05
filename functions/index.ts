3b69babaf537aa2ebdd717bd8dc9c49f4f7125d4
theckert <thomas.james.eckert@gmail.com>
2020-05-04 20:35:54 -0700

    Add makeguid function

--- functions/index.ts
+++ functions/index.ts
@@ -1,5 +1,6 @@
 import { formatDate } from "./formatDate";
 import slugify from "./slugify";
+import makeGuid from "./makeGuid";
 import { validatePostStructure } from "./validatePostStructure";
 
-export { formatDate, slugify, validatePostStructure };
+export { formatDate, slugify, makeGuid, validatePostStructure };
