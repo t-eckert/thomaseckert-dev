f1ab6598ad14d7ab67f640f856c1c7e425d3fe93
theckert <thomas.james.eckert@gmail.com>
2020-05-07 23:44:41 -0700

    Add post viewing route

---
+++ pages/_post.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <pre>
+      {{ postMetadata }}
+    </pre>
+    <pre>
+      {{ postContent }}
+    </pre>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import axios from "axios";
+import { host, routes } from "@/constants";
+
+export default Vue.extend({
+  name: "ThePostRoute",
+
+  async asyncData({ params }) {
+    const { data: postMetadata } = await axios.get(
+      host + routes.POSTS_METADATA + params.post
+    );
+    const { data: postContent } = await axios.get(
+      host + routes.POSTS_CONTENT + params.post
+    );
+    return {
+      postMetadata,
+      postContent,
+    };
+  },
+});
+</script>
