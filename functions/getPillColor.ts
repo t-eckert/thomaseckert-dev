ef394d0d76c4e820272b739050927baa33be15df
theckert <thomas.james.eckert@gmail.com>
2020-05-11 00:06:50 -0700

    Add History tag

--- functions/getPillColor.ts
+++ functions/getPillColor.ts
@@ -2,6 +2,7 @@ const pillColorMap = new Map<string, string>([
   ["Python", "blue"],
   ["Programming", "blue"],
   ["Math", "orange"],
+  ["History", "green"],
 ]);
 
 const getPillColor = (text: string): string => pillColorMap.get(text) || "blue";
