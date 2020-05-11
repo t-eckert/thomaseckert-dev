65d723dbe857010e0f62e9d60b32de2174bc500f
theckert <thomas.james.eckert@gmail.com>
2020-05-10 23:52:03 -0700

    Add markdown formatting

---
+++ functions/formatMarkdown.ts
@@ -0,0 +1,17 @@
+import remark from "remark";
+// @ts-ignore This package lacks a declaration file and is only used here
+import html from "remark-html";
+
+const formatMarkdown = (markdown: string): string => {
+  let out;
+
+  remark()
+    .use(html)
+    .process(markdown, (err: any, file: any) => {
+      out = String(file);
+    });
+
+  return out || "";
+};
+
+export default formatMarkdown;
