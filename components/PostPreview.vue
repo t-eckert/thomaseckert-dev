b44c9cd7b92acd91646f2449db8f22ac8128653c
theckert <thomas.james.eckert@gmail.com>
2020-05-10 23:52:16 -0700

    Use Pill component in Post Preview

--- components/PostPreview.vue
+++ components/PostPreview.vue
@@ -1,20 +1,18 @@
 <template>
-  <section>
+  <section class="px-4">
     <h3>{{ postMetadata.emoji }}</h3>
     <h2 class="mb-2">
       <nuxt-link :to="'/' + postMetadata.slug">{{
         postMetadata.title
       }}</nuxt-link>
     </h2>
-    <div class="flex mb-2">
-      <div class="mr-2">{{ formatDate(postMetadata.publishDate) }}</div>
-      <div
+    <div class="flex items-baseline mb-2">
+      <div class="mr-4">{{ formatDate(postMetadata.publishDate) }}</div>
+      <Pill
         v-for="(tag, index) in postMetadata.tags"
+        :text="tag"
         :key="index"
-        class="mr-2 pill pill-blue"
-      >
-        {{ tag }}
-      </div>
+      />
     </div>
     <p>{{ postMetadata.preview }}</p>
   </section>
@@ -22,11 +20,14 @@
 
 <script lang="ts">
 import Vue from "vue";
+import Pill from "~/components/Pill.vue";
 import { formatDate } from "@/functions";
 import { PostMetadata } from "@/interfaces";
 
 export default Vue.extend({
   name: "PostPreview",
+
+  components: { Pill },
 
   props: {
     postMetadata: {
