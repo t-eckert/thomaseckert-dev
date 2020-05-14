a5f5b8215490b1b0746f2f49e434745fffcec0c9
theckert <thomas.james.eckert@gmail.com>
2020-05-13 21:48:50 -0700

    Create admin and login pages

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>Login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "LoginPage",
+
+  mounted() {
+    this.$store.commit("ui/SET_BREADCRUMBS", [
+      { emoji: "🏡", name: "Home", link: "/" },
+      { emoji: "🔑", name: "Login", link: "/login" },
+    ]);
+  },
+});
+</script>
