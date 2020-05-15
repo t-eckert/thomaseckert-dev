c5afda4e769413f960800e2cfdb4a47cf2dbc32f
theckert <thomas.james.eckert@gmail.com>
2020-05-14 21:33:15 -0700

    Fix post route

--- constants/index.ts
+++ constants/index.ts
@@ -4,6 +4,5 @@ export const getHost = (): string =>
     : "http://localhost:3000";
 
 export const routes = {
-  POSTS_METADATA: "/api/posts/metadata/",
-  POSTS_CONTENT: "/api/posts/content/",
+  POSTS: "/api/posts/",
 };
