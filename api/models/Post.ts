30db467d0bc2809b153eaed18eed4b01af5fdb57
theckert <thomas.james.eckert@gmail.com>
2020-05-14 21:32:25 -0700

    Consolidate Mongoose models

---
+++ api/models/Post.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import connection from "../utils/database-handler";
+
+const PostSchema = new mongoose.Schema(
+  {
+    slug: String,
+    title: String,
+    publishDate: Date,
+    tags: Array,
+    emoji: String,
+    isPublished: Boolean,
+    preview: String,
+    markdown: String,
+  },
+  { collection: "Posts" }
+);
+
+// Prevent duplication of PostMetadataModel
+if (connection.modelNames().includes("Post")) {
+  connection.deleteModel("Post");
+}
+
+const PostModel = mongoose.model("PostMetadata", PostSchema);
+
+export default PostModel;
