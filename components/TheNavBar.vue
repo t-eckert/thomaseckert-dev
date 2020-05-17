c41acf329b7373c7068878b2936b857a4e7f349b
theckert <thomas.james.eckert@gmail.com>
2020-05-17 15:58:18 -0700

    Unify "link-display" for general use, impl in TheNavBar

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,17 +1,21 @@
 <template>
   <nav class="p-2 flex justify-between">
     <ul class="flex flex-wrap">
-      <li v-for="(crumb, index) in breadcrumbs" :key="index">
+      <li
+        v-for="(crumb, index) in breadcrumbs"
+        :key="index"
+        class="link-display"
+      >
         {{ crumb.emoji }}
         <nuxt-link :to="crumb.link">{{ crumb.name }}</nuxt-link>
         <span>&#47;</span>
       </li>
     </ul>
     <div v-if="$auth.loggedIn" class="flex content-center">
-      <nuxt-link to="/admin" class="no-underline mr-4 pill pill-red">
+      <nuxt-link to="/admin" class="link-display no-underline mr-2 pill-red">
         Admin
       </nuxt-link>
-      <span @click="$auth.logout()" class="cursor-pointer">Log out</span>
+      <span @click="$auth.logout()" class="link-display">Log out</span>
     </div>
   </nav>
 </template>
