dd927223bfd5e13c091beb11deb1bf2f21a83750
theckert <thomas.james.eckert@gmail.com>
2020-05-17 15:57:05 -0700

    Allow PUT to post routes

--- api/routes/posts.ts
+++ api/routes/posts.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import PostModel from "../models/Post";
+import verify from "../utils/verify";
+import { Post } from "~/interfaces";
 
 const postsRouter = express.Router();
 
@@ -22,6 +24,30 @@ postsRouter.get("/:slug", async (req, res) => {
     res.send(post);
   } catch (error) {
     res.status(404).send("Could not find post metadata");
+  }
+});
+
+/** Create or overwrite post */
+postsRouter.put("/", verify, async (req, res) => {
+  const post = <Post>req.body;
+
+  try {
+    const postModel = new PostModel({
+      slug: post.slug,
+      title: post.title,
+      publishDate: post.publishDate.toString(),
+      tags: post.tags,
+      emoji: post.emoji,
+      isPublished: post.isPublished,
+      preview: post.preview,
+      markdown: post.markdown,
+    });
+
+    await postModel.save();
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
   }
 });
 
