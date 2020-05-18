5b045984b2ff5a6a5f5f6cc4a4d0361af9d0ebc0
theckert <thomas.james.eckert@gmail.com>
2020-05-18 08:57:12 -0700

    Add form for adding bookmark

---
+++ components/forms/FormAddBookmark.vue
@@ -0,0 +1,29 @@
+<template>
+  <form>
+    <div class="flex justify-between mb-4">
+      <label for="category">Category</label>
+      <input type="url" name="category" id="category" />
+    </div>
+    <div class="flex justify-between mb-4">
+      <label for="link">Link</label>
+      <input type="url" name="link" id="link" />
+    </div>
+    <div class="flex justify-between mb-4">
+      <label for="link-text">Link text</label>
+      <input type="text" name="link-text" id="link-text" />
+    </div>
+    <div class="flex justify-between mb-4">
+      <label for="description" class="mr-4">Description</label>
+      <input type="text" name="description" id="description" />
+    </div>
+    <button>Add</button>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "FormAddBookmark",
+});
+</script>
