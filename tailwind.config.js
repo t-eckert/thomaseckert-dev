50a1edb8222202fd0c90a1fba1775ad0b509d0cd
theckert <thomas.james.eckert@gmail.com>
2020-05-17 19:22:16 -0700

    Set tailwind purge to false

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
+  purge: false,
   theme: {},
   variants: {},
-  plugins: []
-}
+  plugins: [],
+};
