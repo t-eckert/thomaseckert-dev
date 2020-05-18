0ffe4313d0225734a618e8b33863b8c781e6e2f8
theckert <thomas.james.eckert@gmail.com>
2020-05-18 08:57:22 -0700

    Add table view for Bookmarks

---
+++ components/tables/TheBookmarksTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(category, index) in bookmarkCategories" :key="index">
+        <td>
+          {{ category.name }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { BookmarkCategory } from "~/interfaces";
+
+export default Vue.extend({
+  name: "TheBookmarksTable",
+
+  props: {
+    bookmarkCategories: Array as () => BookmarkCategory[],
+  },
+});
+</script>
