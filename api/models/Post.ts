906fbc580672ec535b51e65b405820186e0da800
theckert <thomas.james.eckert@gmail.com>
2020-05-30 16:09:52 -0700

    Add documentation to Post

--- api/models/Post.ts
+++ api/models/Post.ts
@@ -1,17 +1,42 @@
 import mongoose from "mongoose";
 import connection from "../utils/database-handler";
 
+/** `Post` schema
+ *
+ * Used by Mongoose for interfacing with MongoDB
+ */
 const PostSchema = new mongoose.Schema(
   {
+    /** Unique GUID identifying the post */
     _id: String,
+
+    /** Defines the URL path to the post
+     *
+     * Derived from the title as all lowercase with dashes replacing spaces */
     slug: String,
+
+    /** Emoji used for decorating the post */
     emoji: String,
+
+    /** Title of the post */
     title: String,
+
+    /** Date when the post was created */
     createdOn: Date,
+
+    /** Date when the post was last changed */
     updatedOn: Date,
+
+    /** Tags for categorizing the post (e.g. Python, Programming, Science) */
     tags: Array,
+
+    /** Whether the post is listed on the homepage */
     isPublished: Boolean,
+
+    /** Text describing the post */
     preview: String,
+
+    /** Body of the post, formatted in markdown */
     markdown: String,
   },
   { collection: "Posts" }
@@ -22,6 +47,7 @@ if (connection.modelNames().includes("Post")) {
   connection.deleteModel("Post");
 }
 
+// Create the model from schema
 const PostModel = mongoose.model("Post", PostSchema);
 
 export default PostModel;
--- interfaces/Post.ts
+++ interfaces/Post.ts
@@ -1,13 +1,40 @@
+/** `Post` interface
+ *
+ * Expected structure of posts
+ *
+ * Reflected in `api/models/Post.ts` for db interface
+ */
 interface Post {
+  /** Unique GUID identifying the post */
   _id: string;
+
+  /** Defines the URL path to the post
+   *
+   * Derived from the title as all lowercase with dashes replacing spaces */
   slug: string;
+
+  /** Emoji used for decorating the post */
   emoji: string;
+
+  /** Title of the post */
   title: string;
+
+  /** Date when the post was created */
   createdOn: Date;
+
+  /** Date when the post was last changed */
   updatedOn: Date;
+
+  /** Tags for categorizing the post (e.g. Python, Programming, Science) */
   tags: string[];
+
+  /** Whether the post is listed on the homepage */
   isPublished: boolean;
+
+  /** Text describing the post */
   preview: string;
+
+  /** Body of the post, formatted in markdown */
   markdown: string;
 }
 
