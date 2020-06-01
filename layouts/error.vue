8b21e1b1bd470e010c404453f03b99cd7b429d19
theckert <thomas.james.eckert@gmail.com>
2020-05-31 19:52:03 -0700

    Add gradient, shiny text, and display

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container h-center">
     <div v-if="error.statusCode === 404" class="mt-16 mb-8">
-      <h1 class="text-center display-1">404</h1>
+      <h1 class="text-center display-1 shiny">404</h1>
     </div>
     <div v-else class="mt-16 mb-8">
       <h1 class="text-center">An error occurred</h1>
     </div>
-    <div class="text-center mb-4">It's going to be ok.</div>
+    <div class="text-center mb-6">It's going to be ok.</div>
     <div class="link-display h-center w-fit">
       <nuxt-link to="/">🏡 Return home</nuxt-link>
     </div>
