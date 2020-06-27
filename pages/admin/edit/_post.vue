d813dad9e6835e9073ca5253ca46155b66826aa5
theckert <thomas.james.eckert@gmail.com>
2020-06-26 20:20:49 -0700

    Widescreen text editor

--- pages/admin/edit/_post.vue
+++ pages/admin/edit/_post.vue
@@ -1,48 +1,29 @@
 <template>
-  <div class="container">
-    <div class="grid grid-cols-4 gg-4 py-4 v-fill">
-      <div class="card self-start min-w-full">
-        <div class="flex flex-col">
-          <label for="title" class="mb-2">Title</label>
-          <input
-            v-model="post.title"
-            type="text"
-            name="title"
-            id="title"
-            class="mb-4"
-          />
-        </div>
-        <div class="flex flex-col">
-          <label for="tags" class="mb-2">Tags</label>
-          <input
-            v-model="post.tags"
-            type="text"
-            name="tags"
-            id="tags"
-            class="mb-4"
-          />
-        </div>
-      </div>
-
-      <div class="col-span-2">
+  <div>
+    <div class="container">
+      <div class="pt-2 editor-height">
         <TheEditor v-model="post.markdown" class="min-h-full" />
       </div>
-      <div class="card self-end min-w-full flex flex-col">
-        <code class="mb-2"
-          >{{ calculateReadingTime(post.markdown) }} minute(s) read</code
-        >
-        <code class="mb-2">{{ post.markdown.length }} chars</code>
-        <div class="mb-4">
-          <input
-            v-model="post.isPublished"
-            type="checkbox"
-            name="is-published"
-            id="is-published"
-          />
-          <label for="is-published">Published</label>
-        </div>
-        <button @click="save()" class="btn-small">Save</button>
+    </div>
+    <div class="editor-bar flex justify-between align-baseline py-2 px-4">
+      <div>
+        <code>{{ calculateReadTime(post.markdown) }}</code>
       </div>
+      <div>
+        <input
+          type="text"
+          name="emoji"
+          class="magic-input-emoji"
+          :value="post.emoji"
+        />
+        <input
+          type="text"
+          name="title"
+          class="magic-input-title mnw-36"
+          :value="post.title"
+        />
+      </div>
+      <div><button class="btn-small">Save</button></div>
     </div>
   </div>
 </template>
@@ -53,6 +34,7 @@ import axios from "axios";
 import { routes } from "~/constants";
 import TheEditor from "~/components/TheEditor.vue";
 import PostView from "~/components/PostView.vue";
+import { calculateReadTime } from "~/functions";
 
 export default Vue.extend({
   middleware: "auth",
@@ -94,11 +76,8 @@ export default Vue.extend({
       const post = this.post;
       //this.$axios.post(routes.POSTS);
     },
-
-    calculateReadingTime(text: string): number {
-      const wordsPerMinute = 200;
-      const numberOfWords = text.split(" ").length;
-      return Math.ceil(numberOfWords / wordsPerMinute);
+    calculateReadTime(text: string): string {
+      return calculateReadTime(text);
     },
   },
 });
