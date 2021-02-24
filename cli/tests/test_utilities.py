561b5cc270dd8e5c08876cf6d0764f6e372a19fe
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-02-23 21:52:29 -0800

    Test comma separated transaction

--- cli/tests/test_utilities.py
+++ cli/tests/test_utilities.py
@@ -1,4 +1,4 @@
-from te.utilities import format_as_slug
+from te.utilities import comma_separated_to_list, format_as_slug
 
 import pytest
 
@@ -7,6 +7,15 @@ import pytest
 def test_format_as_slug(string: str, expected: str):
     # When
     actual = format_as_slug(string)
+
+    # Then
+    assert expected == actual
+
+
+@pytest.mark.parametrize("comma_separated,expected", [["value 1, value 2", ["value 1", "value 2"]]])
+def test_comma_separated_to_list(comma_separated: str, expected: list):
+    # When
+    actual = comma_separated_to_list(comma_separated)
 
     # Then
     assert expected == actual
