6a8e870cd2d27a08c139b15ae1921d20f7938c2e
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-02-23 17:53:52 -0800

    Rename TheNavBar to Navigation

---
+++ site/components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav>
+        <ul>
+            <li v-for="navLink in navLinks" :key="navLink" class="link-display">
+                <nuxt-link :to="navLink.path">{{ navLink.name }}</nuxt-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "Navigation",
+
+    data() {
+        return {
+            navLinks: [
+                {
+                    name: "Home",
+                    path: "/",
+                },
+                {
+                    name: "Writing",
+                    path: "/posts",
+                },
+            ],
+        };
+    },
+});
+</script>
