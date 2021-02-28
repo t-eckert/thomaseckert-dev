758469ddfb1a4f6e0be3dd3d67043b715c0a485b
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-02-28 11:07:22 -0800

    Hardcode the CLI ROOT_DIR

--- cli/te/config.py
+++ cli/te/config.py
@@ -7,7 +7,7 @@ mode: Optional[str] = os.getenv("TE_MODE")
 
 if mode == "production":
     URL: str = "https://thomaseckert.dev/api"
-    ROOT_DIR: Path = Path("./dir/")
+    ROOT_DIR: Path = Path("/home/t_eck/writing")
 else:  # Test mode
     URL: str = ""
     ROOT_DIR: Path = Path("/home/t_eck/code/thomaseckert-dev/cli/tests/test-dir/")
