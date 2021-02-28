6d68a2747483efc23e626abcf8c3da0c98a82dcd
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-02-28 10:27:40 -0800

    Return 403 if not verified

--- site/api/utils/verify.ts
+++ site/api/utils/verify.ts
@@ -1,12 +1,13 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
+/** Verifies that a request has a valid JWT in the header */
 const verify = (req: Request, res: Response, next: NextFunction) => {
     const token: string | undefined = req.header("token");
     const tokenSecret = process.env.TOKEN_SECRET;
 
     if (token === undefined || tokenSecret === undefined) {
-        res.status(401).send("Access denied");
+        res.status(403).send("Access denied");
     } else {
         try {
             const verified = jwt.verify(token, tokenSecret);
@@ -14,7 +15,7 @@ const verify = (req: Request, res: Response, next: NextFunction) => {
                 next();
             }
         } catch (err) {
-            res.status(401).send("Access denied");
+            res.status(403).send("Access denied");
         }
     }
 };
