b055169db975ed5e3c6b847a3c4df251f57c48b9
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-02-28 17:18:26 -0800

    Remove bookmarks from TheIntroduction

--- site/components/TheIntroduction.vue
+++ site/components/TheIntroduction.vue
@@ -1,36 +1,28 @@
 <template>
     <section>
         <h1>Thomas Eckert</h1>
-        <div class="introduction">
-            <div class="introduction--links">
-                <div class="link-display">
-                    <nuxt-link to="/bookmarks">✨ Bookmarks</nuxt-link>
-                </div>
-            </div>
-            <section>
-                <p>
-                    I am a software engineer on Azure at Microsoft with a
-                    background in Nuclear Physics.
-                </p>
-                <p>
-                    You can find me on
-                    <a target="_blank" href="https://twitter.com/thomaseckert_"
-                        >Twitter</a
-                    >,
-                    <a target="_blank" href="https://github.com/t-eckert"
-                        >GitHub</a
-                    >,
-                    <a
-                        target="_blank"
-                        href="https://www.linkedin.com/in/thomas-eckert/"
-                        >LinkedIn</a
-                    >, and
-                    <a target="_blank" href="https://www.twitch.tv/thomaseckert"
-                        >Twitch</a
-                    >.
-                </p>
-            </section>
-        </div>
+        <section>
+            <p>
+                I am a software engineer on Azure at Microsoft with a background
+                in Nuclear Physics.
+            </p>
+            <p>
+                You can find me on
+                <a target="_blank" href="https://twitter.com/thomaseckert_"
+                    >Twitter</a
+                >,
+                <a target="_blank" href="https://github.com/t-eckert">GitHub</a
+                >,
+                <a
+                    target="_blank"
+                    href="https://www.linkedin.com/in/thomas-eckert/"
+                    >LinkedIn</a
+                >, and
+                <a target="_blank" href="https://www.twitch.tv/thomaseckert"
+                    >Twitch</a
+                >.
+            </p>
+        </section>
         <hr />
     </section>
 </template>
