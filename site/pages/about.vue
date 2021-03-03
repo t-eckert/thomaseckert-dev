2cb3696d3c49d0403d5101e24c2bd67c1ee5b85c
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-02 16:34:29 -0800

    Remove About page cruft

--- site/pages/about.vue
+++ site/pages/about.vue
@@ -1,77 +1,33 @@
 <template>
-    <div class="container">
-        <div>
-            <div>
-                <img
-                    src="/self.jpg"
-                    alt="Thomas and Winona at the park."
-                    class="w-fill object-contain"
-                />
-            </div>
-            <div class="row-start-2 col-start-1 pt-8">
-                <div class="link-display mb-2">
-                    <a
-                        target="_blank"
-                        href="mailto:thomas.james.eckert@gmail.com"
-                        >✉&nbsp;Email</a
-                    >
-                </div>
-                <div class="link-display mb-2">
-                    <a target="_blank" href="https://twitter.com/thomaseckert_"
-                        >🐦 Twitter</a
-                    >
-                </div>
-                <div class="link-display mb-2">
-                    <a target="_blank" href="https://github.com/t-eckert"
-                        >👨‍💻 GitHub</a
-                    >
-                </div>
-                <div class="link-display mb-2">
-                    <a
-                        target="_blank"
-                        href="https://www.linkedin.com/in/thomas-eckert/"
-                        >📘 LinkedIn</a
-                    >
-                </div>
-                <div class="link-display mb-2">
-                    <a target="_blank" href="https://www.twitch.tv/thomaseckert"
-                        >🎥 Twitch</a
-                    >
-                </div>
-            </div>
-            <div class="row-start-2 col-start-2 col-span-2">
-                <p>
-                    Hi 👋, my name is Thomas Eckert. I live in Seattle,
-                    Washington with my wife,
-                    <a target="_blank" href="https://sophiaross.me/">Sophia</a>,
-                    and our corgi, Winona.
-                </p>
-                <p>
-                    I studied physics, focusing on nuclear interactions for
-                    fusion diagnostics. Much of this work involved programming
-                    simulations in C++. I wrote a
-                    <a
-                        target="_blank"
-                        href="https://www.houghton.edu/wp-content/uploads/2019/02/physics-thesis-thomas-eckert-2016.pdf"
-                        >thesis</a
-                    >
-                    and a
-                    <a
-                        target="_blank"
-                        href="https://arxiv.org/pdf/1707.09375.pdf"
-                        >paper</a
-                    >
-                    about it.
-                </p>
-                <p>
-                    After completing my Masters of Physics at the University of
-                    Rochester, I moved to Seattle and started working at
-                    Microsoft as an engineer. Today, I build platform tooling
-                    for defining SLOs on Azure.
-                </p>
-            </div>
-            <div class="row-start-2 col-start-4"></div>
-        </div>
+    <div class="container about">
+        <h1>Hi 👋</h1>
+        <p>
+            My name is Thomas Eckert. I live in Seattle, Washington with my
+            wife,
+            <a target="_blank" href="https://sophiaross.me/">Sophia</a>, and our
+            corgi, Winona.
+        </p>
+        <p>
+            I studied physics, focusing on nuclear interactions for fusion
+            diagnostics. Much of this work involved programming simulations in
+            C++. I wrote a
+            <a
+                target="_blank"
+                href="https://www.houghton.edu/wp-content/uploads/2019/02/physics-thesis-thomas-eckert-2016.pdf"
+                >thesis</a
+            >
+            and a
+            <a target="_blank" href="https://arxiv.org/pdf/1707.09375.pdf"
+                >paper</a
+            >
+            about it.
+        </p>
+        <p>
+            After completing my Masters of Physics at the University of
+            Rochester, I moved to Seattle and started working at Microsoft as an
+            engineer. Today, I build platform tooling for defining SLOs on
+            Azure.
+        </p>
     </div>
 </template>
 
