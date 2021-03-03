c5aabdfe5525679617b94250603dd44b46313774
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-02 16:35:21 -0800

    Add the filterPosts function

--- site/functions/index.ts
+++ site/functions/index.ts
@@ -1,4 +1,5 @@
 import calculateReadTime from "./calculateReadTime";
+import filterPosts from "./filterPosts";
 import formatDate from "./formatDate";
 import formatMarkdown from "./formatMarkdown";
 import formatTimespan from "./formatTimespan";
@@ -6,6 +7,7 @@ import getPillColor from "./getPillColor";
 
 export {
     calculateReadTime,
+    filterPosts,
     formatDate,
     formatMarkdown,
     formatTimespan,
