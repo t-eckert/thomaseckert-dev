c4f510fb6d4d893955214a1ba9e9551ef37682e9
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-03-02 16:33:02 -0800

    Rename TheIntroduction to Introduction

---
+++ site/components/Introduction.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="introduction">
+        <h1>Thomas Eckert</h1>
+        <section>
+            <p>
+                I am a software engineer on Azure at Microsoft with a background
+                in Nuclear Physics.
+            </p>
+            <p>
+                You can find me on
+                <a
+                    target="_blank"
+                    noreferrer
+                    href="https://twitter.com/thomaseckert_"
+                >
+                    Twitter</a
+                >,
+                <a
+                    target="_blank"
+                    noreferrer
+                    href="https://github.com/t-eckert"
+                >
+                    GitHub</a
+                >,
+                <a
+                    target="_blank"
+                    noreferrer
+                    href="https://www.linkedin.com/in/thomas-eckert/"
+                >
+                    LinkedIn</a
+                >, and
+                <a
+                    target="_blank"
+                    noreferrer
+                    href="https://www.twitch.tv/thomaseckert"
+                >
+                    Twitch</a
+                >.
+            </p>
+        </section>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    name: "Introduction",
+});
+</script>
