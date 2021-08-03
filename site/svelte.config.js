b2f7f954b8930110b245eac8ba179933897f127b
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-08-02 22:25:54 -0400

    Update build to use static adapter

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,15 +1,13 @@
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
+export default {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
-export default config;
