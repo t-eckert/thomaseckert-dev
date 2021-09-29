667bebd7d777701569f537a93f4c8af488d92fa1
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-09-28 23:08:38 -0400

    Add Netlify config

--- site/svelte.config.js
+++ site/svelte.config.js
@@ -1,14 +1,16 @@
 import preprocess from 'svelte-preprocess';
+const netlify = require('@sveltejs/adapter-netlify')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [preprocess({
-        postcss: true
-    })],
+		postcss: true
+	})],
 
 	kit: {
+		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
