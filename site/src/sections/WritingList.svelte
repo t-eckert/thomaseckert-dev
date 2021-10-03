363a198aa9b1c46907f1a3e0ba97132609f539aa
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-10-03 15:26:14 -0400

    ReplaceNBSP in writing list

--- site/src/sections/WritingList.svelte
+++ site/src/sections/WritingList.svelte
@@ -1,13 +1,16 @@
 <script>
-	import Tag from '../components/Tag.svelte';
+	import Tag from "../components/Tag.svelte"
+	import replaceNBSP from "../functions/replaceNBSP"
 
-	export let writings;
+	export let writings
 </script>
 
-<section class="flex flex-col gap-1">
+<section class="flex flex-col gap-4 sm:gap-1">
 	{#each writings as writing}
-		<div class="flex flex-row justify-between items-center">
-			<a class="transition hover:underline" href={'/writing/' + writing.slug}>{writing.title}</a>
+		<div class="flex flex-col sm:flex-row sm:justify-between sm:items-center">
+			<a class="transition hover:underline leading-tight" href={"/writing/" + writing.slug}
+				>{replaceNBSP(writing.title)}</a
+			>
 			<div class="flex flex-row gap-1">
 				{#each writing.tags as tag}
 					<Tag {tag} />
