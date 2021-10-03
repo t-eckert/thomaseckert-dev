6e8efe189c308bc809e3add90ddb068798bd9f23
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-10-03 16:14:47 -0400

    Improve writing styling on mobile

--- site/src/routes/writing/[slug].svelte
+++ site/src/routes/writing/[slug].svelte
@@ -1,23 +1,23 @@
 <script context="module">
-	import marked from 'marked';
+	import marked from "marked"
 
-	import Date from '../../components/Date.svelte';
-	import Tag from '../../components/Tag.svelte';
-	import fetchWriting from '../../actions/fetchWriting';
+	import Date from "../../components/Date.svelte"
+	import Tag from "../../components/Tag.svelte"
+	import fetchWriting from "../../actions/fetchWriting"
 
 	export async function load({ page }) {
-		const writing = (await fetchWriting(page.params.slug))[0];
-		return { props: { writing } };
+		const writing = (await fetchWriting(page.params.slug))[0]
+		return { props: { writing } }
 	}
 </script>
 
 <script>
-	import { fade } from 'svelte/transition';
+	import { fade } from "svelte/transition"
 
-	export let writing;
+	export let writing
 
-	let headerHeight;
-	let scrolled;
+	let headerHeight
+	let scrolled
 </script>
 
 <svelte:window bind:scrollY={scrolled} />
@@ -25,15 +25,18 @@
 {#if scrolled > headerHeight}
 	<div
 		transition:fade
-		class="z-20 fixed top-0 w-screen pt-2 flex justify-center font-medium text-gray-900"
+		class="pointer-events-none z-20 fixed top-0 w-screen pt-2 px-2 flex justify-start sm:justify-center font-medium text-gray-900"
 	>
 		{writing.title}
 	</div>
 {/if}
 
-<header class="pt-12 pb-8 mx-auto max-w-4xl flex flex-col gap-2" bind:clientHeight={headerHeight}>
-	<h1 class="text-6xl tracking-tight leading-tight font-semibold">
-		{writing.title.replace(/\s/g, ' ')}
+<header
+	class="px-2 pt-4 sm:pt-12 pb-8 mx-auto max-w-4xl flex flex-col gap-2"
+	bind:clientHeight={headerHeight}
+>
+	<h1 class="text-3xl sm:text-6xl tracking-tight leading-tight font-semibold">
+		{writing.title.replace(/\s/g, " ")}
 	</h1>
 	<div class="flex flex-row gap-1">
 		<Date date={writing.updatedAt} />
@@ -43,6 +46,6 @@
 	</div>
 </header>
 
-<main class="markdown">
+<main class="markdown px-2 mb-24">
 	{@html marked(writing.content)}
 </main>
