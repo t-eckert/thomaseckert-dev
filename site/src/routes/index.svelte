f7ee5ff4004e55ae51e22e556b34270be44c247c
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-10-03 16:15:16 -0400

    Mobile nav looks nice now

--- site/src/routes/index.svelte
+++ site/src/routes/index.svelte
@@ -1,24 +1,27 @@
 <script context="module">
 	import fetchWritings from '../actions/fetchWritings';
 
-  export async function load() {
-		const writings = await fetchWritings()
-    return { props: { writings }}
-  }
+	export async function load() {
+		const writings = await fetchWritings();
+		return { props: { writings } };
+	}
 </script>
 
 <script>
-	import Introduction from "../sections/Introduction.svelte";
-	import WritingList from "../sections/WritingList.svelte"
+	import Introduction from '../sections/Introduction.svelte';
+	import WritingList from '../sections/WritingList.svelte';
 
-	export let writings
+	export let writings;
 </script>
 
 <svelte:head>
 	<title>Thomas Eckert</title>
 </svelte:head>
 
-<section class="mx-auto pt-12 max-w-3xl flex flex-col gap-8">
-	<Introduction/>
-	<WritingList {writings}/>
+<section class="mx-auto px-2 pt-4 sm:pt-12 max-w-3xl flex flex-col gap-8">
+	<Introduction />
+	<div class="flex flex-col gap-2">
+		<h2 class="font-medium">Recent Writings</h2>
+		<WritingList {writings} />
+	</div>
 </section>
--- site/src/sections/Nav.svelte
+++ site/src/sections/Nav.svelte
@@ -1,20 +1,52 @@
+<script>
+	import { fade, blur, fly, slide, scale, draw, crossfade } from "svelte/transition"
+
+	import Nav from "../components/Nav.svelte"
+
+	const smBreakpoint = 640
+
+	let pageWidth
+
+	$: navIsOpen = false
+</script>
+
+<svelte:window bind:innerWidth={pageWidth} />
+
 <section
-	class="z-10 sticky top-0 border-b border-gray-50 bg-white bg-opacity-40 backdrop-filter backdrop-blur-lg"
+	class="z-10 h-10 sticky top-0 border-b border-gray-50 bg-white bg-opacity-40 backdrop-filter backdrop-blur-lg"
 >
-	<nav class="mx-auto max-w-7xl py-2 flex flex-row justify-between items-center">
-		<a
-			class="text-sm font-medium px-2 py-0.5 rounded-xl text-gray-700 animate hover:text-gray-900 hover:bg-gray-100"
-			href="/">Thomas Eckert</a
-		>
-		<div class="flex flex-row gap-1 items-center">
-			<a
-				class="text-sm font-medium px-2 py-0.5 rounded-xl text-gray-700 animate hover:text-gray-900 hover:bg-gray-100"
-				href="/writing">Writing</a
+	{#if pageWidth < smBreakpoint}
+		<div class="px-2 w-full h-full flex flex-row justify-end">
+			<button
+				class={[
+					"relative cursor-pointer rounded-md p-0.5 self-center transition hover:bg-gray-200",
+					navIsOpen && "bg-gray-100"
+				].join(" ")}
+				on:click={() => (navIsOpen = !navIsOpen)}
 			>
-			<a
-				class="text-sm font-medium px-2 py-0.5 rounded-xl text-gray-700 animate hover:text-gray-900 hover:bg-gray-100"
-				href="/about">About</a
-			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</button>
+			{#if navIsOpen}
+				<div
+					transition:fly={{ y: -20, duration: 200 }}
+					class="absolute right-2 mt-12 p-1 bg-white border border-gray-50 rounded-xl shadow"
+				>
+					<Nav />
+				</div>
+			{/if}
 		</div>
-	</nav>
+	{:else}
+		<Nav />
+	{/if}
 </section>
