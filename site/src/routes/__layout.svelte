56f8f192ca2f6be5533f8db37c94679a81a3e0c1
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-10-05 21:18:49 -0400

    Better Fathom tracking

--- site/src/routes/__layout.svelte
+++ site/src/routes/__layout.svelte
@@ -1,6 +1,19 @@
 <script>
+	import { onMount } from "svelte"
+	import { browser } from "$app/env"
+	import { page } from "$app/stores"
+	import * as Fathom from "fathom-client"
+
 	import "../app.postcss"
 	import NavBar from "../sections/NavBar.svelte"
+
+	onMount(() => {
+		Fathom.load("JUVSFRPV", {
+			includedDomains: ["thomaseckert.dev"]
+		})
+	})
+
+	$: $page.path, browser && Fathom.trackPageview()
 </script>
 
 <NavBar />
