eba808b54a9a82d10c9c8d5b9daec9e6c9a2d250
Thomas Eckert <thomas.james.eckert@gmail.com>
2021-10-05 20:45:28 -0400

    Show a new token for new writings

--- site/src/sections/WritingList.svelte
+++ site/src/sections/WritingList.svelte
@@ -1,17 +1,27 @@
 <script>
+	import Date from "../components/Date.svelte"
 	import Tag from "../components/Tag.svelte"
 	import replaceNBSP from "../functions/replaceNBSP"
+	import isNew from "../functions/isNew"
 
 	export let writings
 </script>
 
 <section class="flex flex-col gap-4 sm:gap-2">
 	{#each writings as writing}
-		<div class="flex flex-col sm:flex-row sm:justify-between sm:items-center">
-			<a class="transition hover:underline leading-tight" href={"/writing/" + writing.slug}
-				>{replaceNBSP(writing.title)}</a
-			>
+		<div class="flex flex-col">
+			<div class="flex flex-row items-center gap-1">
+				{#if isNew(writing.publishedAt)}
+					<span class="px-1 py-0.5 rounded-md font-medium text-xs text-blue-800 bg-blue-200"
+						>New!</span
+					>
+				{/if}
+				<a class="transition hover:underline leading-tight" href={"/writing/" + writing.slug}
+					>{replaceNBSP(writing.title)}</a
+				>
+			</div>
 			<div class="flex flex-row gap-1">
+				<Date date={writing.updatedAt} />
 				{#each writing.tags as tag}
 					<Tag {tag} />
 				{/each}
