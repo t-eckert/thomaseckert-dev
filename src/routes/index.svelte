4ccd8b592eb846220696055d8cb1d1ef302d0dcd
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-01-02 20:16:09 -0500

    Add site code notes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -21,6 +21,7 @@
 </svelte:head>
 
 <div class="mx-auto px-2 pt-4 sm:pt-40 max-w-6xl flex flex-col gap-6">
+	<!-- Intro -->
 	<section class="pb-6 sm:pb-12 flex flex-col max-w-sm gap-4">
 		<h1 class="text-6xl font-semibold">Hi,</h1>
 		<p>
@@ -36,6 +37,7 @@
 	</section>
 
 	<div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
+		<!-- Writing -->
 		<section class="flex flex-col gap-2">
 			<h2 class="font-medium text-4xl">Some writings</h2>
 			{#each writings as writing}
@@ -43,8 +45,9 @@
 			{/each}
 		</section>
 
-		<section>
-			<p class="max-w-sm">
+		<section class="max-w-md flex flex-col gap-4">
+			<!-- Socials -->
+			<p>
 				You can also find me on <a
 					href="https://twitter.com/thomaseckert_"
 					target="blank_"
@@ -56,7 +59,12 @@
 					href="https://www.linkedin.com/in/thomas-eckert/"
 					target="blank_"
 					rel="noopener noreferrer">LinkedIn</a
-				>. The source code for this site is in
+				>.
+			</p>
+
+			<!-- Site code -->
+			<p>
+				The source code for this site is in
 				<a
 					href="https://github.com/t-eckert/thomaseckert-dev"
 					target="blank_"
@@ -64,10 +72,12 @@
 				>
 					this repo
 				</a>
-				if you see something you'd like to borrow.
+				if you see something you'd like to borrow. It is built using SvelteKit, TailwindCSS, and deployed
+				on Netlify.
 			</p>
 		</section>
 
+		<!-- Bookmarks -->
 		<section class="flex flex-col gap-4 sm:gap-2">
 			<h2 class="font-medium text-4xl">Recent bookmarks</h2>
 			{#each bookmarks as bookmark}
