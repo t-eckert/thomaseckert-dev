01a203e69206cf015cd398c10d0c3d6c27031097
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-01-08 16:00:50 -0500

    Improve look and feel of bookmarks

--- src/routes/bookmarks.svelte
+++ src/routes/bookmarks.svelte
@@ -9,37 +9,44 @@
 </script>
 
 <script>
-	import BookmarkCategory from "../components/BookmarkCategory.svelte"
+	import BookmarkCategory from "../sections/BookmarkCategory.svelte"
 
 	export let bookmarkCategories
 
 	$: allExpanded = true
 </script>
 
-<section class="mx-auto px-2 flex flex-col max-w-7xl">
-	<header class="my-4 sm:my-16 flex flex-col gap-2 sm:gap-4">
+<svelte:head>
+	<title>Thomas Eckert: Bookmarks</title>
+</svelte:head>
+
+<div
+	class="fixed -top-24 -left-10 opacity-5 -rotate-6 bg-gradient-to-b from-rose-500 to-rose-400 pointer-events-none w-[1800px] h-96"
+/>
+
+<section class="mx-auto px-4 flex flex-col max-w-6xl">
+	<header class="my-4 sm:my-12 flex flex-col">
 		<h1 class="font-semibold text-2xl sm:text-6xl text-gray-900">Bookmarks</h1>
 
-		<div class="flex flex-row justify-between">
-			<p class="text-gray-700">Some of my favorite things across the Internet.</p>
+		<div class="w-full flex flex-row justify-between">
+			<p class="ml-1 text-gray-700 text-sm">Some of my favorite things across the Internet.</p>
 			{#if allExpanded}
 				<button
-					class="font-medium transition text-gray-600 hover:text-gray-800"
+					class="font-medium text-sm transition text-gray-600 hover:text-gray-800"
 					on:click={() => (allExpanded = false)}>Minimize all</button
 				>
 			{:else}
 				<button
-					class="font-medium transition text-gray-600 hover:text-gray-800"
+					class="font-medium text-sm transition text-gray-600 hover:text-gray-800"
 					on:click={() => (allExpanded = true)}>Expand all</button
 				>
 			{/if}
 		</div>
 	</header>
-	<div
-		class="max-w-7xl mx-auto w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-10"
-	>
+
+	<main class="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
 		{#each bookmarkCategories as bookmarkCategory}
 			<BookmarkCategory {...bookmarkCategory} isExpanded={allExpanded} />
 		{/each}
-	</div>
+	</main>
 </section>
