b7b383bb3cee75f73c72930e01112b43d12d5042
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-01-17 23:11:53 -0500

    Set up nesting for postcss

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,18 +1,18 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
+const cssnano = require("cssnano")
 
-const mode = process.env.NODE_ENV;
-const dev = mode === "development";
+const mode = process.env.NODE_ENV
+const dev = mode === "development"
 
 const config = {
 	plugins: [
-		tailwindcss(),
-		autoprefixer(),
-		!dev && cssnano({
-			preset: "default",
+		require("tailwindcss/nesting"),
+		require("tailwindcss"),
+		require("autoprefixer"),
+		!dev &&
+		cssnano({
+			preset: "default"
 		})
-	],
-};
+	]
+}
 
-module.exports = config;
+module.exports = config
