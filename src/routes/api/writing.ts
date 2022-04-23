37a9377902a7ceeddda822b6031d464bb59343b3
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-04-23 10:49:51 -0400

    Try running from static json

--- src/routes/api/writing.ts
+++ src/routes/api/writing.ts
@@ -1,8 +1,12 @@
 import pMap from "p-map"
 
+import staticWriting from "../../../static/writing.json"
+
 import type Writing from "src/types/Writing"
 
 export async function get({ url }: { url: URL }) {
+	const host = url.origin
+
 	const takeParam = url.searchParams.get("take")
 	const take = takeParam ? parseInt(takeParam) : undefined
 
@@ -12,40 +16,44 @@ export async function get({ url }: { url: URL }) {
 	const modules = import.meta.glob("../writing/*.{svx,svelte}")
 
 	// Fetch, filter, and structure the writing
-	const writing: Writing[] = (await pMap(
-		Object.entries(modules),
-		async function ([filename, module]) {
-			const { metadata } = await module()
-
-			return {
-				title: metadata?.title,
-				slug: filename.replace("../writing/", "").replace(".svx", "").replace(".svelte", ""),
-				published: metadata?.published ? new Date(metadata.published) : undefined,
-				tags: metadata?.tags || [],
-			}
-		}
-	))
-		// Only published writing
-		.filter(({ published }) => published !== undefined)
-		// Only writing with the specified tags
-		.filter(({ tags: writingTags }) => {
-			if (tags.length === 0) {
-				return true
-			}
-			if (writingTags.length === 0) {
-				return false
-			}
-			return writingTags.some((tag: string) => tags.includes(tag))
-		})
-		// Sort by published date
-		.sort((a, b) => {
-			if (!a.published || !b.published) {
-				return 0
-			}
-			return a.published < b.published ? 1 : -1
-		})
-		// Take the specified number of writing
-		.slice(0, take)
+	const writing: Writing[] =
+		(staticWriting as any[])
+			/* This was failing on Netlify
+			(await pMap(
+				Object.entries(modules),
+				async function ([filename, module]) {
+					const { metadata } = await module()
+		
+					return {
+						title: metadata?.title,
+						slug: filename.replace("../writing/", "").replace(".svx", "").replace(".svelte", ""),
+						published: metadata?.published ? new Date(metadata.published) : undefined,
+						tags: metadata?.tags || [],
+					}
+				}
+			))
+			*/
+			// Only published writing
+			.filter(({ published }) => published !== undefined)
+			// Only writing with the specified tags
+			.filter(({ tags: writingTags }) => {
+				if (tags.length === 0) {
+					return true
+				}
+				if (writingTags.length === 0) {
+					return false
+				}
+				return writingTags.some((tag: string) => tags.includes(tag))
+			})
+			// Sort by published date
+			.sort((a, b) => {
+				if (!a.published || !b.published) {
+					return 0
+				}
+				return a.published < b.published ? 1 : -1
+			})
+			// Take the specified number of writing
+			.slice(0, take)
 
 	return {
 		body: { writing, tags, take },
