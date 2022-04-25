90ec7a0e71e7f4b02d8732d377c6037fbeea5dd8
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-04-24 23:28:50 -0400

    Add Fathom and fade between pages

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,8 +2,23 @@
 	import { onMount } from "svelte"
 	import { fade } from "svelte/transition"
 	import { page } from "$app/stores"
+	import * as Fathom from "fathom-client"
 
 	import "src/app.css"
+
+	onMount(() => {
+		Fathom.load("JUVSFRPV", { includedDomains: ["thomaseckert.dev"] })
+	})
 </script>
 
-<slot />
+<svelte:head><title>Thomas Eckert</title></svelte:head>
+
+<div class="selection:bg-gray-700 selection:text-white">
+	<body class="bg-gray-50 min-h-screen overflow-x-hidden pb-20">
+		{#key $page.url.pathname}
+			<section in:fade={{ duration: 200 }}>
+				<slot />
+			</section>
+		{/key}
+	</body>
+</div>
