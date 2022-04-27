1d6d931009c48d396fe86bcb3c3fc602c8d2d67d
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-04-26 22:54:36 -0400

    Add Bar, Link, and Nav components

---
+++ src/components/Bar.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import Nav from "src/components/Nav.svelte"
+
+	const breakpoint = 640
+
+	let width: number
+
+	$: open = true
+</script>
+
+<svelte:window bind:innerWidth={width} />
+
+<section
+	class="z-50 sticky top-0 w-full border-b border-gray-50 bg-white bg-opacity-40 backdrop-filter backdrop-blur-lg"
+>
+	<div class="mx-auto max-w-6xl p-1 flex flex-row justify-between items-center">
+		<a href="/" class="text-md sm:text-xs font-medium text-gray-700 animate hover:text-gray-900">
+			Thomas Eckert
+		</a>
+		{#if width < breakpoint}
+			<button
+				class="relative cursor-pointer rounded-md p-0.5 self-center transition hover:bg-gray-200"
+				on:click={() => (open = !open)}
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-6 w-6"
+					viewBox="0 0 20 20"
+					fill="currentColor"
+				>
+					<path
+						fill-rule="evenodd"
+						d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</button>
+		{:else}
+			<div class="text-xs font-medium flex flex-row gap-2">
+				<Nav />
+			</div>
+		{/if}
+	</div>
+</section>
+
+{#if open && width < breakpoint}
+	<section class="fixed bottom-8 w-full flex flex-row items-center justify-center">
+		<div
+			class="flex flex-col items-center rounded-sm border px-12 gap-2 py-4 text-md font-medium border-gray-50 bg-white bg-opacity-40 backdrop-filter backdrop-blur-lg"
+		>
+			<Nav />
+		</div>
+	</section>
+{/if}
---
+++ src/components/Link.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	type LinkStyle = "none" | "external" | "writing" | "bookmarks" | "notes" | "projects" | "about"
+
+	const classStyle = (style: LinkStyle): string => {
+		switch (style) {
+			case "none":
+				return "decoration-gray-700"
+			case "external":
+				return "decoration-sky-600"
+			case "writing":
+				return "decoration-rose-600"
+			case "bookmarks":
+				return "decoration-emerald-600"
+			case "notes":
+				return "decoration-violet-600"
+			case "projects":
+				return "decoration-yellow-600"
+			case "about":
+				return "decoration-gray-600"
+		}
+	}
+
+	export let href: string,
+		style: LinkStyle = "none"
+</script>
+
+<a
+	{href}
+	class={`font-medium underline decoration-2  text-gray-900  transition ${classStyle(style)}`}
+>
+	<slot />
+</a>
