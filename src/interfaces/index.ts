e312b512e1ce08f77f8a4e64071f7e89af773022
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-04-26 23:01:11 -0400

    Stub out interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,6 @@
+import type { Bookmark } from "./Bookmark"
+import type { Note } from "./Note"
+import type { Project } from "./Project"
+import type { Writing } from "./Writing"
+
+export { Bookmark, Note, Project, Writing }
