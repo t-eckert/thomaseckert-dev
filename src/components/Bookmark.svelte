2fe43a852e6ca1c5c609af3d6dc0ac754a474ac7
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-04-27 22:52:35 -0400

    Add Bookmark and Date components

---
+++ src/components/Bookmark.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let description: string, link: string
+</script>
+
+<a
+	href={link}
+	target="blank_"
+	rel="noopener noreferrer"
+	class="font-medium transition underline decoration-2 decoration-rose-400 text-gray-900 hover:text-rose-600 hover:decoration-rose-500"
+>
+	{description}
+</a>
---
+++ src/components/Date.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	export let date: Date
+</script>
+
+<span>{new Date(date).toDateString()}</span>
