2508733a853abd32d7f4aa228b3fdadb502e196f
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-04-27 22:48:42 -0400

    Add Tag component

---
+++ src/components/Tag.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const resourceClass = new Map([
+		["writing", "bg-sky-100 text-sky-900 hover:bg-sky-200"],
+		["note", "bg-emerald-100 text-emerald-900 hover:bg-emerald-200"]
+	])
+
+	export let tag: string,
+		resource = "tags"
+</script>
+
+<a
+	href={resource + "?tags=" + tag}
+	class={`decoration-transparent px-1 py-0.5 text-xs hover:decoration-transparent  ${resourceClass.get(
+		resource
+	)}`}>{tag}</a
+>
