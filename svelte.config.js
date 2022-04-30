d7434cadc86d0ebb0fc95e4ba1653e675cac6e76
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-04-29 22:37:57 -0400

    Add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,30 @@
 // @filename: index.js
 import path from "path"
+import { mdsvex } from "mdsvex"
 
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+const mdsvexConfig = {
+	"extensions": [".md", ".svx"],
+	"smartypants": {
+		"dashes": "oldschool"
+	},
+	"remarkPlugins": [],
+	"rehypePlugins": [],
+	"layout": {
+		"writing": "./src/layouts/writing.svelte",
+		"notes": "./src/layouts/notes.svelte",
+	}
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	extensions: [".svelte", ...mdsvexConfig.extensions],
+
+	preprocess: [preprocess({
+		postcss: true
+	}), mdsvex(mdsvexConfig)],
 
 	kit: {
 		adapter: adapter(),
