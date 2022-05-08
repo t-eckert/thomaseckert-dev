038c9dbc5e2a9ab05a780ce7a300e02b8d381ffe
Thomas Eckert <thomas.james.eckert@gmail.com>
2022-05-08 17:06:19 -0400

    Fix notion token leak

--- src/notion.ts
+++ src/notion.ts
@@ -2,5 +2,5 @@ import { Client } from "@notionhq/client"
 
 // Initializing a client
 export const notion = new Client({
-    auth: "secret_4cekGbGJbkz60fdHr8KCKG5CX6hTfGFWtxknQn2L0Ud" // process.env.NOTION_TOKEN,
+    auth: import.meta.env.VITE_NOTION_TOKEN,
 })
